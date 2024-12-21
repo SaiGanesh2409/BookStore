88bf19f28281624449a4d4426f5e7223a01a5ac5
Sai Ganesh <gsai9960@gmail.com>
2024-12-21 16:35:38 +0530

    User Completed

--- online-bookstore/src/main/java/com/onlineBookStore/service/UserService.java
+++ online-bookstore/src/main/java/com/onlineBookStore/service/UserService.java
@@ -1,14 +1,22 @@
 package com.onlineBookStore.service;
 
+import java.util.List;
+
 import com.onlineBookStore.dto.UserDTO;
 
 public interface UserService {
 
 	UserDTO getUserById(Long id);
+	
+	List<UserDTO> getAllUsers();
 
 	UserDTO createUser(UserDTO userDTO);
 	
 	boolean isEmailExist(String email);
 	
 	boolean isUserNameExist(String userName);
+	
+	void deleteUser(Long id);
+	
+	UserDTO updateUser(Long id, UserDTO userDTO);
 }
